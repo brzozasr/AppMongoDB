093934620c9f3a5286a243cf94eaf7f9a4ce47e4
Slawomir Brzozowski <slawomir.brzozowski@avanade.com>
2021-08-21 11:01:15 +0200

    Moq data partially implemented

--- AppMongoDB/Controllers/MongoController.cs
+++ AppMongoDB/Controllers/MongoController.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Mvc;
 using AppMongoDB.Data;
+using AppMongoDB.Models.Movie;
 
 namespace AppMongoDB.Controllers
 {
     public class MongoController : ApiController
     {
-        private readonly IMongoRepository _mongoRepository;
+        private readonly IMongoRepository<Movie> _mongoRepository;
 
-        public MongoController(IMongoRepository mongoRepository)
+        public MongoController(IMongoRepository<Movie> mongoRepository)
         {
             _mongoRepository = mongoRepository;
         }
 
         // GET api/<controller>
-        public IEnumerable<Car> Get()
+        public async Task<IHttpActionResult> Get()
         {
-            return _mongoRepository.GetData();
+            return Ok(_mongoRepository.GetAllData());
         }
 
         // GET api/<controller>/5
--- AppMongoDB/Data/MoqMongoRepository.cs
+++ AppMongoDB/Data/MoqMongoRepository.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http.Results;
 using AppMongoDB.Models.Movie;
+using MongoDB.Bson;
 
 namespace AppMongoDB.Data
 {
@@ -13,12 +15,32 @@ namespace AppMongoDB.Data
 
         public MoqMongoRepository()
         {
-            
+            Random rnd = new Random();
+            MoqData.Add(
+            new Movie
+                {
+                    MovieId = ObjectId.GenerateNewId(),
+                    Title = "High and Dizzy",
+                    Description = "A tipsy doctor encounters his patient sleepwalking on a building ledge, high above the street.",
+                    FullDescription = "After a long wait, a young doctor finally has a patient come to his office. She is a young woman whose father has brought her to be treated for sleep-walking, but the father becomes annoyed with the doctor, and takes his daughter away. Soon afterward, the young doctor shares in a drinking binge with another doctor who has built a still in his office. After a series of misadventures, the two of them wind up in the same hotel where the daughter and her father are staying, leading to some hazardous predicaments.",
+                    Released = new DateTime(rnd.Next(1950, 2021), rnd.Next(1, 13), rnd.Next(1, 29))
+            });
+            MoqData.Add(
+                new Movie
+                {
+                    MovieId = ObjectId.GenerateNewId(),
+                    Title = "High and Dizzy",
+                    Description = "A tipsy doctor encounters his patient sleepwalking on a building ledge, high above the street.",
+                    FullDescription = "After a long wait, a young doctor finally has a patient come to his office. She is a young woman whose father has brought her to be treated for sleep-walking, but the father becomes annoyed with the doctor, and takes his daughter away. Soon afterward, the young doctor shares in a drinking binge with another doctor who has built a still in his office. After a series of misadventures, the two of them wind up in the same hotel where the daughter and her father are staying, leading to some hazardous predicaments.",
+                    Released = new DateTime(rnd.Next(1950, 2021), rnd.Next(1, 13), rnd.Next(1, 29))
+                });
         }
 
-        public Task<IList<Movie>> GetAllData()
+        public async Task<IList<Movie>> GetAllData()
         {
-            throw new NotImplementedException();
+            var task = Task.Run(() => MoqData);
+            await Task.CompletedTask;
+            return task.Result;
         }
 
         public Task<Movie> GetById(long id)
